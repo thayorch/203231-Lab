c9cd6b589b6965d3cb11c662ff6a13837507186d
thayorch <thatcharnon.10210@gmail.com>
2025-08-10 15:37:49 +0700

    SSR

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get("/lab/:id", (req, res) => {
+    const fileName = `lab${req.params.id}.html`;
+    res.sendFile(path.join(__dirname, 'public', fileName), err => {
+        if (err) {
+            res.status(404).send('Lab not found');
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
