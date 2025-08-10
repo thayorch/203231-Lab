6c8886b1481c6abbeab9a90f6a1921175b8cc643
thayorch <thatcharnon.10210@gmail.com>
2025-08-10 16:13:38 +0700

    comment path err

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get("/lab/:id", (req, res) => {
-    const fileName = `lab${req.params.id}.html`;
-    res.sendFile(path.join(__dirname, 'public', fileName), err => {
-        if (err) {
-            res.status(404).send('Lab not found');
-        }
-    });
-});
+// app.get("/lab/:id", (req, res) => {
+//     const fileName = `lab${req.params.id}.html`;
+//     res.sendFile(path.join(__dirname, 'public', fileName), err => {
+//         if (err) {
+//             res.status(404).send('Lab not found');
+//         }
+//     });
+// });
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
